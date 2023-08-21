d4a700032fafb2ce3e402656e8688cb163890879
Ephraim Jan Conales <conalesej@gmail.com>
2023-08-21 21:57:04 +0800

    checkpoint: Part 3 - Pages & Routes

---
+++ app/tickets/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const News = () => {
+  return (
+    <main>
+      <span className="text-orange-500 text-5xl">Tickets</span>
+    </main>
+  );
+};
+
+export default News;
