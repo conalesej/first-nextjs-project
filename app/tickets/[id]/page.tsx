a88d148aa0e177e68fe85d645643d9b6df5d467a
Ephraim Jan Conales <conalesej@gmail.com>
2023-08-22 19:30:38 +0800

    checkpoint: Part 8 - Static Rendering

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,5 +1,24 @@
 import { ITickets } from "@/app/utils/types";
+import { notFound } from "next/navigation";
 import React from "react";
+
+export const dynamicParams = false; // if(false) Returns a 404 page if an id hasn't been pre-rendered before OTHERWISE Next js will refetch this shit
+
+export const generateStaticParams = async () => {
+  {
+    /** 
+    If the revalidate is set to 0 -> Then this will be useless because this is set to the cache
+  */
+  }
+
+  const res = await fetch("http://localhost:4000/tickets/");
+  const tickets: ITickets[] = await res.json();
+
+  const ticketsId = tickets.map((ticket) => ({ id: ticket.id }));
+
+  // [{id: 1}, {id: 2}, {id: 3}, ...]
+  return ticketsId;
+};
 
 interface ITicketDetails {
   params: { id: number };
@@ -11,6 +30,10 @@ const getTicket = async (id: number) => {
       revalidate: 30, //  use 0 to opt out of using cache
     },
   });
+
+  if (!res.ok) {
+    notFound();
+  }
   return res.json();
 };
 const TicketDetails: React.FC<ITicketDetails> = async ({ params }) => {
