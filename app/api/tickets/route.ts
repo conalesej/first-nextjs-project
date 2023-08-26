12ac919f87cabb3831e03c26cf94501dc48836a3
Ephraim Jan Conales <conalesej@gmail.com>
2023-08-26 20:39:15 +0800

    checkpoint: Part 19 - Dynamic vs Static Route Handlers

--- app/api/tickets/route.ts
+++ app/api/tickets/route.ts
@@ -1,11 +1,13 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic"; // Any router handler that is declared in this file will be dynamic and not static (cached)
+
 export const GET = async () => {
   try {
-    const res = await fetch("http://localhost:4000/tickets");
-
+    const res = await fetch("http://localhost:4000/tickets", {
+      next: { revalidate: 0 }, // So it would not be cached
+    });
     const tickets = await res.json();
-
     return NextResponse.json(tickets, {
       status: 200,
     });
