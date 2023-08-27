22330195a77d4a3046e1b2f8eda0078f6928b2c7
Ephraim Jan Conales <conalesej@gmail.com>
2023-08-27 17:26:23 +0800

    checkpoint: Part 24 - Signing up a new User

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,20 +1,43 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import AuthForm from "../AuthForm";
+import { useRouter } from "next/navigation";
 
 const Signup = () => {
+  const router = useRouter()
+  const [error, setError] = useState("");
   const handleSubmit = async (
     e: React.FormEvent<HTMLFormElement>,
     email: string,
     password: string
   ) => {
     e.preventDefault();
-    console.log("user signup", { email, password });
+    const supabase = createClientComponentClient();
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback/`,
+      },
+    });
+
+    if (error) {
+      setError(error.message);
+    }
+    if (!error) {
+      router.push('/verify')
+    }
   };
   return (
     <main>
-      <AuthForm handleSubmit={handleSubmit} />
       <h2 className="text-center">Signup</h2>
+      <AuthForm handleSubmit={handleSubmit} />
+      {error && (
+        <>
+          <div className="error">{error}</div>
+        </>
+      )}
     </main>
   );
 };
