11b913878a707b119acfcfc7d641d03568f47f6f
Ephraim Jan Conales <conalesej@gmail.com>
2023-09-02 20:19:40 +0800

    checkpoing: Part 40 - Handling a POST Request in a Server Action

--- app/(dashboard)/tickets/action.ts
+++ app/(dashboard)/tickets/action.ts
@@ -1,5 +1,22 @@
 "use server";
 
-export const addTicket = async (e: any) => {
-  console.log("Hello World");
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export const addTicket = async (formData: FormData) => {
+  const ticket = Object.fromEntries(formData);
+  const supabase = createServerActionClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  //    insert data
+  const { error } = await supabase
+    .from("tickets")
+    .insert({ ...ticket, user_email: session?.user.email });
+
+  revalidatePath("/tickets");
+  redirect("/tickets");
 };
--- app/(dashboard)/tickets/create/CreateForm.tsx
+++ app/(dashboard)/tickets/create/CreateForm.tsx
@@ -1,64 +1,28 @@
 "use client";
 
-import { IPriority } from "@/app/utils/types";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { addTicket } from "../action";
 
 const CreateForm = () => {
   const router = useRouter();
-  const [title, setTitle] = useState<string>("");
-  const [body, setBody] = useState<string>("");
-  const [priority, setPriority] = useState<IPriority>("low");
+
   const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setIsLoading(true);
-
-    const newTicket = { title, body, priority };
-    const res = await fetch("http://localhost:3000/api/tickets", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newTicket),
-    });
-
-    const json = await res.json();
-
-    if (json.error) {
-      console.log(json.error.message);
-    } else {
-      router.refresh();
-      router.push("/tickets");
-    }
-  };
 
   return (
     // <form action="" className="w-1/2" onSubmit={handleSubmit}>
     <form action={addTicket} className="w-1/2">
       <label>
         <span>Title:</span>
-        <input
-          required
-          type="text"
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-        />
+        <input name="title" required type="text" />
       </label>
       <label>
         <span>Body:</span>
-        <textarea
-          required
-          onChange={(e) => setBody(e.target.value)}
-          value={body}
-        />
+        <textarea name="body" required />
       </label>
       <label>
         <span>Priority:</span>
-        <select
-          onChange={(e) => setPriority(e.target.value as IPriority)}
-          value={priority}
-        >
+        <select name="priority">
           <option value="low">Low Priority</option>
           <option value="medium">Medium Priority</option>
           <option value="high">High Priority</option>
